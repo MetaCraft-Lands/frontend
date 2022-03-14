7a7e643f26e832b70e810851613d9b83704682e3
Sharu <jsrshark110@gmail.com>
2022-03-13 17:22:12 -0700

    Remove redundant files

--- src/Components/WalletLogin/Login.tsx
+++ src/Components/WalletLogin/Login.tsx
@@ -1,33 +1,13 @@
 import { useState } from "react";
-import Dropdown from "react-bootstrap/esm/Dropdown";
 import { Span } from "../Header/styles";
-import {nearWallet, signIn} from "../../libs/wallet";
+
 declare var window: any;
 
 // Wallet login.
 const Login = () => {
   const [defaultAccount, setDefaultAccount] = useState(null);
 
-  const [accountId, setAccountId] = useState(nearWallet.getAccountId());
-
-  const signInNear = () => {
-    signIn().then(
-        (accountId) => {
-          console.log("log in wallet.tsx: ", accountId);
-          setAccountId(accountId);
-        },
-        (err) => {
-          console.error(err);
-        }
-    );
-  };
-
-  const signOutNear = () => {
-    nearWallet.signOut();
-    setAccountId("");
-  };
-
-  const signInMetaMask = () => {
+  const signInOnClick = () => {
     console.log("sign in metamask")
     if (window.ethereum) {
       window.ethereum
@@ -42,32 +22,16 @@ const Login = () => {
     setDefaultAccount(newAccount);
   };
 
-  const signOutMetaMask = () => {
+  const signOutOnClick = () => {
     setDefaultAccount(null);
   };
 
   if (defaultAccount) {
     return (
-      <Span onClick={signOutMetaMask}>{defaultAccount} (logout)</Span>
-    );
-  } else if(accountId) {
-    return (
-        <Span onClick={signOutNear}>{accountId} (logout)</Span>
+      <Span onClick={signOutOnClick}>{defaultAccount} (logout)</Span>
     );
   }
-  return (<>
-
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Connect to Wallet
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={signInNear} >Connect to Near </Dropdown.Item>
-          <Dropdown.Item onClick={signInMetaMask}>Connect To Metamask</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-  </>)
+  return <Span onClick={signInOnClick}> Connect with MetaMask</Span>;
 };
 
 export default Login;
