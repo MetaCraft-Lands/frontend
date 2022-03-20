995c20e5527cf71bbe2611f9acc77d6424f4f363
Sharu <jsrshark110@gmail.com>
2022-03-20 16:02:28 -0700

    Play to earn

--- src/libs/contracts.tsx
+++ src/libs/contracts.tsx
@@ -1,26 +1,29 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-import contract from '../contractAbi/MetaCraft.json';
+import nftAbi from "../contractAbi/MetaCraft.json";
+import tokenAbi from "../contractAbi/Build.json";
 
-const contractAddr = "0xdE0344a9B4f138406a72c2d9B82F596351ef061a";
-const abi = contract.abi;
+const NFT_CONTRACT_ADDR = "0xdE0344a9B4f138406a72c2d9B82F596351ef061a";
+const TOKEN_CONTRACT_ADDR = "0x91f4141C818beaEaA867F003ccFD3612C84654A6";
+
 declare var window: any;
 const { ethereum } = window;
 
 const getNftContract = () => {
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          return new ethers.Contract(contractAddr, abi, signer);
-}
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  return new ethers.Contract(NFT_CONTRACT_ADDR, nftAbi.abi, signer);
+};
 
 // Globle contract singleton instance shared by many components.
 const nftContract = getNftContract();
 
-const tokenContract = null;
+const getTokenContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  return new ethers.Contract(TOKEN_CONTRACT_ADDR, tokenAbi.abi, signer);
+};
 
-export {
-  nftContract,
-  tokenContract
-}
+const tokenContract = getTokenContract();
 
-
+export { nftContract, tokenContract };
