a1dbb27fb2c8bf171d613e2a8ee28b9f9b1b5710
Sharu <jsrshark110@gmail.com>
2022-03-20 22:01:36 -0700

    update

--- src/Components/GetStarted/index.tsx
+++ src/Components/GetStarted/index.tsx
@@ -1,104 +1,92 @@
 import {
-
-    Action,
-    ActionBox,
-    ActionButton,
-    ActionDescription,
-    Section,
-    SectionTitle,
-    SubSection,
+  Action,
+  ActionBox,
+  ActionButton,
+  ActionDescription,
+  Section,
+  SectionTitle,
+  SubSection,
 } from "./styles";
 
 import Container from "../Container";
 
 const GetStarted = () => {
-    const nftpage = "/nft"
-    const marketplace = "https://testnets.opensea.io/collection/metacraft-h3ifpy6lki"
-    return (
-        <Section>
-            <Container>
-                <SectionTitle>
-                   Get Started
-                </SectionTitle>
+  const nftpage = "/nft";
+  const marketplace =
+    "https://testnets.opensea.io/collection/metacraft-h3ifpy6lki";
+  return (
+    <Section>
+      <Container>
+        <SectionTitle>Get Started</SectionTitle>
 
-                <SubSection>
-                    <Action>
-                        Get Started
-                    </Action>
-                    <ActionBox>
-                    <ActionDescription>
-                        Mint a land and start to create anything you can imagine. What's more? You own them all!
-                    </ActionDescription>
-                    <ActionButton href={nftpage}>Mint NOW</ActionButton>
-                    </ActionBox>
-                </SubSection>
+        <SubSection>
+          <Action>Get Started</Action>
+          <ActionBox>
+            <ActionDescription>
+              Mint a land and start to create anything you can imagine. What's
+              more? You own them all!
+            </ActionDescription>
+            <ActionButton href={nftpage}>Mint NOW</ActionButton>
+          </ActionBox>
+        </SubSection>
 
-                <SubSection>
-                    <Action>
-                        Explore
-                    </Action>
-                    <ActionBox>
-                    <ActionDescription>
-                        Explore LANDs owned by users to experience incredible Minecraft metaverse
-                    </ActionDescription>
-                    <ActionButton href={nftpage}>Explore NOW</ActionButton>
-                    </ActionBox>
-                </SubSection>
+        <SubSection>
+          <Action>Explore</Action>
+          <ActionBox>
+            <ActionDescription>
+              Explore LANDs owned by users to experience incredible Minecraft
+              metaverse
+            </ActionDescription>
+            <ActionButton href={nftpage}>Explore NOW</ActionButton>
+          </ActionBox>
+        </SubSection>
 
+        <SubSection>
+          <Action>Create</Action>
+          <ActionBox>
+            <ActionDescription>
+              Create buildings, artworks, games and even more using the simple
+              tool we provided. Everyone can create
+            </ActionDescription>
+            <ActionButton href={nftpage}>Build Now</ActionButton>
+          </ActionBox>
+        </SubSection>
 
-                <SubSection>
-                    <Action>
-                        Create
-                    </Action>
-                    <ActionBox>
-                        <ActionDescription>
-                            Create buildings, artworks, games and even more using the simple tool we provided. Everyone can create
-                        </ActionDescription>
-                        <ActionButton href={nftpage}>Build Now</ActionButton>
-                    </ActionBox>
-                </SubSection>
+        <SubSection>
+          <Action>Play & Earn</Action>
+          <ActionBox>
+            <ActionDescription>
+              $BUILD tokens are ERC20 tokens that are generated with in-game
+              activity in Minecraft and staked NFTs. $BUILD can be used for
+              building your LANDs and unlock in-game items. More to come...
+            </ActionDescription>
+            <ActionButton href={nftpage}>Claim $BUILD NOW</ActionButton>
+          </ActionBox>
+        </SubSection>
 
+        <SubSection>
+          <Action>Trade</Action>
+          <ActionBox>
+            <ActionDescription>
+              Trade your NFTs on marketplace or start by renting one. Sure you
+              will be overloaded with amazingggg stuff.
+            </ActionDescription>
+            <ActionButton href={marketplace}>Opensea Marketplace</ActionButton>
+          </ActionBox>
+        </SubSection>
 
-                <SubSection>
-                    <Action>
-                        Play & Earn
-                    </Action>
-                    <ActionBox>
-                    <ActionDescription>
-                        $BUILD tokens are ERC20 tokens that are generated with in-game activity in Minecraft and staked NFTs. $BUILD can be used for building your LANDs and unlock in-game items. More to come...
-                    </ActionDescription>
-                    <ActionButton href={nftpage}>Claim $BUILD NOW</ActionButton>
-                    </ActionBox>
-                </SubSection>
-
-                <SubSection>
-                    <Action>
-                        Know More
-                    </Action>
-                    <ActionBox>
-                    <ActionDescription>
-                        Trade your NFTs on marketplace or start by renting one. Sure you will be overloaded with amazingggg stuff.
-                    </ActionDescription>
-                    <ActionButton href={marketplace}>Opensea Marketplace</ActionButton>
-                    </ActionBox>
-                </SubSection>
-
-                <SubSection>
-                    <Action>
-                        Know More
-                    </Action>
-                    <ActionBox>
-                        <ActionDescription>
-                            Check out our social medias and whitepaper
-                        </ActionDescription>
-                        <ActionButton>Learn More</ActionButton>
-                    </ActionBox>
-               </SubSection>
-
-
-            </Container>
-        </Section>
-    );
+        <SubSection>
+          <Action>Know More</Action>
+          <ActionBox>
+            <ActionDescription>
+              Check out our social medias and whitepaper
+            </ActionDescription>
+            <ActionButton>Learn More</ActionButton>
+          </ActionBox>
+        </SubSection>
+      </Container>
+    </Section>
+  );
 };
 
 export default GetStarted;
--- src/libs/contracts.tsx
+++ src/libs/contracts.tsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import nftAbi from "../contractAbi/MetaCraft.json";
 import tokenAbi from "../contractAbi/Build.json";
 
-const NFT_CONTRACT_ADDR = "0xC7087CD8eC021423dE4E6C5d67858E09c7fDb0b3";
+const NFT_CONTRACT_ADDR = "0x2FBCe257c5f05d649783C83D9ccA3B5006654777";
 const TOKEN_CONTRACT_ADDR = "0x91f4141C818beaEaA867F003ccFD3612C84654A6";
 
 declare var window: any;
