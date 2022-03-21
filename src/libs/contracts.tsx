104209010d01be74dd00c4e67f44bf57ca410910
Sharu <jsrshark110@gmail.com>
2022-03-20 21:33:00 -0700

    update contract

--- src/libs/contracts.tsx
+++ src/libs/contracts.tsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import nftAbi from "../contractAbi/MetaCraft.json";
 import tokenAbi from "../contractAbi/Build.json";
 
-const NFT_CONTRACT_ADDR = "0xdE0344a9B4f138406a72c2d9B82F596351ef061a";
+const NFT_CONTRACT_ADDR = "0xC7087CD8eC021423dE4E6C5d67858E09c7fDb0b3";
 const TOKEN_CONTRACT_ADDR = "0x91f4141C818beaEaA867F003ccFD3612C84654A6";
 
 declare var window: any;
