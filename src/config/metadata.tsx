44b62e1f8f3543c2021cc494d5dcc71f989b642a
Sharu <jsrshark110@gmail.com>
2022-03-20 21:33:34 -0700

    update

--- src/config/metadata.tsx
+++ src/config/metadata.tsx
@@ -1,29 +1,29 @@
-import { BigNumber } from '@ethersproject/bignumber';
-import getHash from './config';
+import { BigNumber } from "@ethersproject/bignumber";
+import getHash from "./config";
 
 const metadata = [
-    {
-        "_tokenId": 1,
-        "_seed": 79311,
-        "_tokenMetadataIPFSHash": "QmY3svVgfvCaRLFs7E2BsFsvsd6YrGF5Bio1tX57iMTMcr"
-    },
-    {
-        "_tokenId": 2,
-        "_seed": 70754,
-        "_tokenMetadataIPFSHash": "QmUfs2wkyFgu5W26q66pNs5S7Rt9wAFShK6WToVQ3kHFc5"
-    },
-    {
-        "_tokenId": 3,
-        "_seed": 39583,
-        "_tokenMetadataIPFSHash": "QmczX6Zh7faUxdCf9z2Zdr5vnQsmh6qdp8wF95KGEtCGtC"
-    },
-]
+  {
+    _tokenId: 1,
+    _seed: 79311,
+    _tokenMetadataIPFSHash: "QmY3svVgfvCaRLFs7E2BsFsvsd6YrGF5Bio1tX57iMTMcr",
+  },
+  // {
+  //     "_tokenId": 2,
+  //     "_seed": 70754,
+  //     "_tokenMetadataIPFSHash": "QmUfs2wkyFgu5W26q66pNs5S7Rt9wAFShK6WToVQ3kHFc5"
+  // },
+  // {
+  //     "_tokenId": 3,
+  //     "_seed": 39583,
+  //     "_tokenMetadataIPFSHash": "QmczX6Zh7faUxdCf9z2Zdr5vnQsmh6qdp8wF95KGEtCGtC"
+  // },
+];
 
 export const randomMeta = () => {
-    const idx = Math.floor(Math.random() * metadata.length);
-    let meta = metadata[idx];
-    return meta;
-}
+  const idx = Math.floor(Math.random() * metadata.length);
+  let meta = metadata[idx];
+  return meta;
+};
 
 export const readUpdatedIpfsMetadataHash = (id: BigNumber): string => {
   return getHash(id.toNumber());
